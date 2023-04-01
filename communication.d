d68c3a87911b2d2800ae87b69abdfee7cbfda17f
Shivam5022 <shivamverma5022@gmail.com>
2023-04-01 21:46:56 +0530

    removed unnecessary

--- communication.d
+++
@@ -1,3 +0,0 @@
-communication.o: communication.c /usr/include/stdc-predef.h types.h \
- defs.h param.h x86.h memlayout.h mmu.h proc.h spinlock.h sleeplock.h \
- communication.h
--- oppo.d
+++
@@ -1,1 +0,0 @@
-oppo.o: oppo.c /usr/include/stdc-predef.h types.h stat.h user.h pstate.h
